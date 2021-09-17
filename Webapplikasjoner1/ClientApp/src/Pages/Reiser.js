53c8a0df4ea1f42f53517850cb99ded64dada75d
Sigurd Jensen <sigjen3@hotmail.com>
2021-09-17 20:06:06 +0200

    Reiser page

--- Webapplikasjoner1/ClientApp/src/Pages/Reiser.js
+++ Webapplikasjoner1/ClientApp/src/Pages/Reiser.js
@@ -1,9 +1,90 @@
 import React from "react";
+import {Table, Card} from "reactstrap";
 
 
 export const Reiser = () => {
     
+    const reiseData = [
+        {
+            id: 1,
+            strekning: 'Oslo-Kiel',
+            antall : 3,
+            pris: 600,
+            dato: '16.09.21'
+        },
+        {
+            id: 2,
+            strekning: 'Sandefjord-Strømstad',
+            antall : 2,
+            pris: 400,
+            dato: '25.08.21'
+        },
+        {
+            id: 3,
+            strekning: 'Kristiansand-Hirtshals',
+            antall : 1,
+            pris: 200,
+            dato: '21.07.21'
+        },
+        {
+            id: 4,
+            strekning: 'Sandefjord-Strømstad',
+            antall : 4,
+            pris: 800,
+            dato: '03.06.21'
+        },
+        {
+            id: 5,
+            strekning: 'Larvik-Hirtshals',
+            antall : 4,
+            pris: 800,
+            dato: '05.02.21'
+        },
+    ];
+    
     return (
-        <p>Dette er dine reiser</p>
+        
+        <div className={'px-5'}>
+            <h2 className={'my-4'}>Dine reiser</h2>
+                <Table className={"table table- table-hover "}>
+                    <thead className={"thead-light"}>
+                        <tr>
+                            <th>Bestilt</th>
+                            <th>Pris</th>
+                            <th>Strekning</th>
+                            <th>Antall</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {reiseData.map((reise) => {
+                            return (
+                                <tr>
+                                    <td>
+                                        <div className={'font-weight-bolder'}>
+                                            {reise.dato}
+                                        </div>
+                                    </td>
+                                    <td>
+                                    <div className={'font-weight-bolder'}>
+                                        {reise.pris},-
+                                    </div>
+                                </td>
+                                    <td>
+                                        <div>
+                                            {reise.strekning}
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div>
+                                            {reise.antall}
+                                        </div>
+                                    </td>
+                                    
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </Table>
+        </div>
     )
 }
