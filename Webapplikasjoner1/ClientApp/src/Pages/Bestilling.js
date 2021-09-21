ef63cc4caf83edfa7a6b6a1d5a73ad135b4069f5
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-09-21 11:16:23 +0200

    Bestillingsside

--- Webapplikasjoner1/ClientApp/src/Pages/Bestilling.js
+++ Webapplikasjoner1/ClientApp/src/Pages/Bestilling.js
@@ -1,13 +1,66 @@
-import React from "react";
-import {Card, CardBody} from "reactstrap";
+import React, {useState} from "react";
+import {Button, Col, Form, FormGroup, Input, Label, Row} from "reactstrap";
+import {StrekningsVelger} from "../components/BestillForm/StrekningsVelger";
 
 export const Bestilling = () => {
+    const [avgangssted, setAvgangssted] = useState('default');
+    const [fraDato, setFraDato] = useState();
+    const [tilDato, setTilDato] = useState();
+    const [retur, setRetur] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    const changeRetur = () => setRetur(!retur);
     
     return (
-        <Card>
-            <CardBody>
-                Bestill ny billett
-            </CardBody>
-        </Card>
+        <Form>
+            <Row form>
+                <Col md={12}>
+                    <p>Reisedetaljer</p>
+                </Col>
+            </Row>
+            <Row form>
+                <StrekningsVelger
+                    avgangssted={avgangssted}
+                    setAvgangssted={setAvgangssted}
+                    setLoading={setLoading}
+                    loading={loading}
+                />
+            </Row>
+            
+            <Row form>
+                <Col md={12}>
+                    <FormGroup check>
+                        <Input type={'checkbox'} onChange={() => changeRetur()} />
+                        <Label>Tur/Retur?</Label>
+                    </FormGroup>
+                </Col>
+            </Row>
+
+            <Row form>
+                <Col md={12}>
+                    <FormGroup>
+                        <Label>Velg avgang</Label>
+                        <Input type={'date'} />
+                    </FormGroup>
+                </Col>
+            </Row>
+
+            <Row form>
+                <Col md={12}>
+                    <FormGroup>
+                        <Label>Velg retur</Label>
+                        <Input type={'date'} disabled={!retur}/>
+                    </FormGroup>
+                </Col>
+            </Row>
+
+            <div className={'mt-3'}>
+                <Button
+                    color={'primary'}
+                >
+                    Neste
+                </Button>
+            </div>
+        </Form>
     )
 }
