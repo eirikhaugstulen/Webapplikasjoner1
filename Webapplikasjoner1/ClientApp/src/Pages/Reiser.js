9c8ba57fa8affe021cfd908e08b776ecd4b6eed3
Sigurd Jensen <sigjen3@hotmail.com>
2021-09-21 11:11:27 +0200

    Første utkast for reiser

--- Webapplikasjoner1/ClientApp/src/Pages/Reiser.js
+++ Webapplikasjoner1/ClientApp/src/Pages/Reiser.js
@@ -3,123 +3,78 @@ import {Table} from "reactstrap";
 
 
 export const Reiser = () => {
-    
+
     const reiseData = [
         {
             id: 1,
             strekning: 'Oslo-Kiel',
-            antall : 3,
+            antall: 3,
             pris: 600,
             dato: '16.09.21'
         },
         {
             id: 2,
             strekning: 'Sandefjord-Strømstad',
-            antall : 2,
+            antall: 2,
             pris: 400,
             dato: '25.08.21'
         },
         {
             id: 3,
             strekning: 'Kristiansand-Hirtshals',
-            antall : 1,
+            antall: 1,
             pris: 200,
             dato: '21.07.21'
         },
         {
             id: 4,
             strekning: 'Sandefjord-Strømstad',
-            antall : 4,
+            antall: 4,
             pris: 800,
             dato: '03.06.21'
         },
         {
             id: 5,
             strekning: 'Larvik-Hirtshals',
-            antall : 4,
-            pris: 800,
-            dato: '05.02.21'
-        },
-        {
-            id: 1,
-            strekning: 'Oslo-Kiel',
-            antall : 3,
-            pris: 600,
-            dato: '16.09.21'
-        },
-        {
-            id: 2,
-            strekning: 'Sandefjord-Strømstad',
-            antall : 2,
-            pris: 400,
-            dato: '25.08.21'
-        },
-        {
-            id: 3,
-            strekning: 'Kristiansand-Hirtshals',
-            antall : 1,
-            pris: 200,
-            dato: '21.07.21'
-        },
-        {
-            id: 4,
-            strekning: 'Sandefjord-Strømstad',
-            antall : 4,
-            pris: 800,
-            dato: '03.06.21'
-        },
-        {
-            id: 5,
-            strekning: 'Larvik-Hirtshals',
-            antall : 4,
+            antall: 4,
             pris: 800,
             dato: '05.02.21'
         },
     ];
-    
     return (
-        
-        <div className={'px-5 h-100'}>
+        <div className={'bg-white shadow-sm'}>
             <h2 className={'my-4'}>Dine reiser</h2>
-                <Table className={'table table- table-hover '}>
-                    <thead className={"thead-light"}>
+            <Table className={'table table- table-hover '}>
+                <thead className={"thead-light"}>
+                <tr>
+                    <th>Bestilt</th>
+                    <th>Pris</th>
+                    <th>Strekning</th>
+                    <th>Antall</th>
+                </tr>
+                </thead>
+                <tbody>
+                {reiseData.map((reise) => {
+                    return (
                         <tr>
-                            <th>Bestilt</th>
-                            <th>Pris</th>
-                            <th>Strekning</th>
-                            <th>Antall</th>
+                            <td>
+                                {reise.dato}
+                            </td>
+                            <td>
+                                {reise.pris},-
+                            </td>
+                            <td>
+
+                                {reise.strekning}
+                            </td>
+                            <td> 
+                                {reise.antall}
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        {reiseData.map((reise) => {
-                            return (
-                                <tr>
-                                    <td>
-                                        <div>
-                                            {reise.dato}
-                                        </div>
-                                    </td>
-                                    <td>
-                                    <div>
-                                        {reise.pris},-
-                                    </div>
-                                </td>
-                                    <td>
-                                        <div>
-                                            {reise.strekning}
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div>
-                                            {reise.antall}
-                                        </div>
-                                    </td>
-                                    
-                                </tr>
-                            )
-                        })}
-                    </tbody>
-                </Table>
+                    )
+                })}
+                </tbody>
+            </Table>
         </div>
     )
 }
