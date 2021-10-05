06298870d3942f676beeb5b0acd3914167af23fc
Magnus Holmen <56063586+kykauwi@users.noreply.github.com>
2021-10-05 12:07:13 +0200

    Fix validering & lagt til rette for returAttributter i Billett.cs (#12)

---
+++ Webapplikasjoner1/Models/Validering.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Webapplikasjoner1.Models
+{
+    public class Validering
+    {
+        public static bool BillettValidering(Billett innBillett)
+        {
+            Regex reg = new Regex(@"[a-zA-ZæøåÆØÅ. \-]{2,20}");
+            Regex regEtternavn = new Regex(@"[a-zA-ZæøåÆØÅ. \-]{2,50}");
+            
+            bool testFrasted = reg.IsMatch(innBillett.FraSted);
+            bool testTilsted = reg.IsMatch(innBillett.TilSted);
+            bool testFornavn = reg.IsMatch(innBillett.Fornavn);
+            bool testEtternavn = regEtternavn.IsMatch(innBillett.Etternavn);
+
+            if (!testFrasted || !testTilsted || !testFornavn || !testEtternavn)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
