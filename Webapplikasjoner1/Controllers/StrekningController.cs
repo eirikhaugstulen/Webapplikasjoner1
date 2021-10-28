3b3b5407dbe2bf6c8cc0b33a1499b8554c457e26
kykauwi <themagnus1208@gmail.com>
2021-10-28 14:59:32 +0200

    Satt opp konstruktør for StrekningController

---
+++ Webapplikasjoner1/Controllers/StrekningController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using ILogger = Serilog.ILogger;
+
+namespace Webapplikasjoner1.Controllers
+{
+    public class StrekningController : ControllerBase
+    {
+        private readonly IStrekningRepository _db;
+        private ILogger<StrekningController> _log;
+    
+    
+        public StrekningController(IStrekningRepository db, ILogger <StrekningController> log)
+        {
+            _db = db;
+            _log = log;
+        }
+    }
+}
