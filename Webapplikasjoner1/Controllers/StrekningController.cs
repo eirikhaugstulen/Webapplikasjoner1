b5832282c10ef9be17b37b3f969ca3ff5f839b80
kykauwi <themagnus1208@gmail.com>
2021-10-28 15:23:27 +0200

    Gjort ferdig basic Controller for Strekning

--- Webapplikasjoner1/Controllers/StrekningController.cs
+++ Webapplikasjoner1/Controllers/StrekningController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Webapplikasjoner1.DAL;
+using Webapplikasjoner1.Models;
 using ILogger = Serilog.ILogger;
 
 namespace Webapplikasjoner1.Controllers
@@ -16,6 +18,45 @@ namespace Webapplikasjoner1.Controllers
         {
             _db = db;
             _log = log;
+        }
+
+        public async Task<ActionResult> LagreStrekning(Strekning innStrekning)
+        {
+            bool returOk = await _db.LagreStrekning(innStrekning);
+
+            if (!returOk)
+            {
+                _log.LogInformation("Strekningen ble ikke lagret");
+                return BadRequest("Strekningen ble ikke lagret");
+            }
+
+            return Ok("Strekning lagret");
+        }
+
+        public async Task<ActionResult> EndreStrekning(Strekning innStrekning)
+        {
+            bool returOk = await _db.EndreStrekning(innStrekning);
+            if (!returOk)
+            {
+                _log.LogInformation("Strekningen ble ikke endret");
+                return BadRequest("Strekningen ble ikke endret");
+            }
+
+            return Ok("Strekning endret");
+        }
+
+        public async Task<ActionResult> SlettStrekning(int id)
+        {
+            bool returOk = await _db.SlettStrekning(id);
+            
+            if (!returOk)
+            {
+                _log.LogInformation("Strekningen ble ikke slettet");
+                return NotFound("Strekningen ble ikke slettet");
+            }
+            
+            return Ok("Strekning slettet");
+            
         }
         
     }
