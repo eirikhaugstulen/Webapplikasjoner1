b4b82385cbc16358cac61b94111252027bd4f7e0
kykauwi <themagnus1208@gmail.com>
2021-10-28 14:48:59 +0200

    Lagt til Strekning klasse

---
+++ Webapplikasjoner1/Models/Strekning.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Webapplikasjoner1.Models
+{
+    public class Strekning
+    {
+        public int Id { get; set; }
+        public string FraSted { get; set; }
+        public string TilSted { get; set; }
+    }
+}
