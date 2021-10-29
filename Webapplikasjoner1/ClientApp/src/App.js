805c21307cd3dc6d39f380dcaddc45b6b8510074
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-10-29 12:52:48 +0200

    Formik & AdminHomePage (#18)

--- Webapplikasjoner1/ClientApp/src/App.js
+++ Webapplikasjoner1/ClientApp/src/App.js
@@ -3,8 +3,9 @@ import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './Pages/Home';
 import { Reiser } from "./Pages/Reiser";
-import { Bestilling } from "./Pages/Bestilling";
+import { Bestilling } from "./Pages/Bestilling/Bestilling";
 import { Kvittering } from "./Pages/Kvittering";
+import {AdminContainer} from "./Pages/Admin/AdminContainer";
 
 
 export default class App extends Component {
@@ -12,12 +13,13 @@ export default class App extends Component {
 
   render () {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path={'/reiser'} component={Reiser} />
-        <Route path={'/bestill'} component={Bestilling} />
-        <Route path={'/kvittering'} component={Kvittering} />
-      </Layout>
+        <Layout>
+            <Route exact path='/' component={Home} />
+            <Route path={'/reiser'} component={Reiser} />
+            <Route path={'/bestill'} component={Bestilling} />
+            <Route path={'/kvittering'} component={Kvittering} />
+            <Route path={'/admin'} component={AdminContainer} />
+        </Layout>
     );
   }
 }
