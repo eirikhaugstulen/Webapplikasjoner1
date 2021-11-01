b2b801ecd77bbfb4cebeaa16bf75e03d11a35dd2
MartinTC123 <martin_candor@hotmail.com>
2021-11-01 17:21:43 +0100

    Opprettet en enhetstest klasse for å enhetsteste LokasjonController.

---
+++ WebAppTest/LokasjonTest.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Webapplikasjoner1.Controllers;
+using Webapplikasjoner1.DAL;
+using Webapplikasjoner1.Models;
+
+namespace WebAppTest
+{
+    public class LokasjonTest
+    {
+        private const string _loggetInn = "loggetInn";
+        private const string _ikkeLoggetInn = "";
+        
+        private readonly Mock<ILokasjonRepository> mockRep = new Mock<ILokasjonRepository>();
+        private readonly Mock<ILogger<LokasjonController>> mockLog = new Mock<ILogger<LokasjonController>>();
+
+        private readonly Mock<HttpContext> mockHttpContext = new Mock<HttpContext>();
+        private readonly MockHttpSession mockSession = new MockHttpSession();
+        
+    }
+}
