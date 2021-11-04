3f8f3485a2520eb3471014353b49c27015784577
kykauwi <themagnus1208@gmail.com>
2021-11-04 10:46:48 +0100

    Oppdatert billett model

--- Webapplikasjoner1/Models/Billett.cs
+++ Webapplikasjoner1/Models/Billett.cs
@@ -5,14 +5,13 @@ namespace Webapplikasjoner1.Models
     public class Billett
     {  
         public int Id { get; set; }
-        public string Fornavn { get; set; }
-        public string Etternavn { get; set; }
         public bool Retur { get; set; }
         public string OrdreNummer { get; set; }
         public string Avgang { get; set; }
         public string Type { get; set; }
         public int TotalPris { get; set; }
         public int Antall { get; set; }
+        public string KundeId { get; set; }
         
     }
 }
