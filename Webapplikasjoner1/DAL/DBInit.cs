bac29f9d9506b7ff9c8102a0b38bdb75f5bc1538
kykauwi <themagnus1208@gmail.com>
2021-11-05 11:27:46 +0100

    Oppdatert db_init

--- Webapplikasjoner1/DAL/DBInit.cs
+++ Webapplikasjoner1/DAL/DBInit.cs
@@ -17,14 +17,20 @@ namespace Webapplikasjoner1.DAL
 
             var lokasjon = new Lokasjoner()
             {
-                StedsNummer = "1",
+                StedsNummer = "cdefg",
                 Stedsnavn = "Oslo",
             };
             
             var lokasjon2 = new Lokasjoner()
             {
-                StedsNummer = "2",
+                StedsNummer = "bcdef",
                 Stedsnavn = "Bergen",
+            };
+
+            var lokasjon3 = new Lokasjoner()
+            {
+                StedsNummer = "abcde",
+                Stedsnavn = "Kristiansand",
             };
 
             db.Lokasjonene.Add(lokasjon);
@@ -32,26 +38,24 @@ namespace Webapplikasjoner1.DAL
 
             var strekning = new Strekninger()
             {
-                StrekningNummer = "12",
+                StrekningNummer = "strin",
                 FraSted = lokasjon,
                 TilSted = lokasjon2,
             };
             
             var strekning1 = new Strekninger()
             {
-                StrekningNummer = "123",
-                FraSted = lokasjon,
-                TilSted = lokasjon2,
+                StrekningNummer = "qopsd",
+                FraSted = lokasjon2,
+                TilSted = lokasjon,
             };
             
             var strekning2 = new Strekninger()
             {
-                StrekningNummer = "1234",
-                FraSted = lokasjon,
+                StrekningNummer = "mkers",
+                FraSted = lokasjon3,
                 TilSted = lokasjon2,
             };
-
-
 
             db.Strekningene.Add(strekning);
             db.Strekningene.Add(strekning1);
@@ -60,37 +64,67 @@ namespace Webapplikasjoner1.DAL
 
             var avgang = new Avganger()
             {
-                AvgangNummer = "1",
-                Dato = "10-12-2021",
+                AvgangNummer = "pqler",
+                Dato = "2021-11-15",
                 Klokkeslett = "23:59",
                 Pris = 100,
                 Strekning = strekning,
             };
 
+            var avgang2 = new Avganger()
+            {
+                AvgangNummer = "mkere",
+                Dato = "2021-12-11",
+                Klokkeslett = "08:10",
+                Pris = 300,
+                Strekning = strekning1,
+            };
+
+            var avgang3 = new Avganger()
+            {
+                AvgangNummer = "lpere",
+                Dato = "2021-12-23",
+                Klokkeslett = "15:00",
+                Pris = 200,
+                Strekning = strekning2,
+            };
+
             db.Avgangene.Add(avgang);
+            db.Avgangene.Add(avgang2);
+            db.Avgangene.Add(avgang3);
 
             var kunde = new Kunder()
             {
                 KundeId = "aaaaa",
                 Adresse = "Skedsmovollen 51",
                 Epost = "none@gmail.com",
-                Fornavn = "per",
-                Etternavn = "hansen",
-                Telefonnummer = "12345678",
+                Fornavn = "Per",
+                Etternavn = "Hansen",
+                Telefonnummer = "004792345678",
             };
             var billett = new Billetter()
             {
-                Id = 0,
                 Retur = true,
-                OrdreNummer = "1",
+                OrdreNummer = "mkrsl",
                 Type = "Student",
                 Antall = 1,
                 Avgang = avgang,
                 TotalPris = 100,
                 KundeId = kunde,
             };
+            var billett2 = new Billetter()
+            {
+                Retur = false,
+                OrdreNummer = "mkrsl",
+                Type = "Student",
+                Antall = 1,
+                Avgang = avgang2,
+                TotalPris = 300,
+                KundeId = kunde,
+            };
 
             db.Billettene.Add(billett);
+            db.Billettene.Add(billett2);
 
             var admin = new Adminer();
             admin.Brukernavn = "Adminbruker";
